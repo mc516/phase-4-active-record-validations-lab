292ecdf1c2d619eff8060cb12b442855ebb8b148
Mickey <mickeyc11111@gmail.com>
2022-11-05 10:41:53 -0400

    commit tree 00cd52fa68a7d57b8ff9ff0063b82828fc991183

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :click_bait
+
+    CLICKBAIT = [
+        "Won't Believe",
+        "Secret",
+        "Top [number]",
+        "Guess"
+    ]
+
+    def click_bait
+        if title.present? && CLICKBAIT.none? { |t| title.match(t) } 
+            errors.add(:title, "Need more click bait!")
+        end
+    end
+   
 end
